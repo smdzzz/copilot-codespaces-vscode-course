59b2d7420eaef1d0d024eae6f9226e85f8de4947
BERBERI SHAMSDEANE <shamso75018@icloud.com>
2026-04-29 08:58:08 +0200

    Add basic web server implementation in comments.js

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web servere('http');
+stname = '127.0.0.1'
+const port = 3000;
+p.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});;
